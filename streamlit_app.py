fb82c36e8554c2cd768c2c60e9a8703c43f9fa39
Nabila Arab <arabnabilapro@gmail.com>
2026-04-02 14:53:02 +0200

    Fix: force pandas 2.1.4 et retire dtype_backend

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-if hasattr(pd.options.mode, "dtype_backend"):
-    pd.options.mode.dtype_backend = "numpy_nullable"
+
 import matplotlib.pyplot as plt
 import io
 
